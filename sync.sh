ab4fe2e163922b005b23c35df04f0b4cacd24488
madisetti <madisetti@jhu.edu>
2019-12-18 23:18:26 -0500

    Provided most recent norebooks

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 while inotifywait -r -e modify,create,delete,move carsmoney; do
-  rsync -r -a -v -e "ssh -oStrictHostKeyChecking=no -i~/.ssh/cs -p$@" --delete carsmoney root@0.tcp.ngrok.io:/usr/local/lib/python3.6/dist-packages/
+  rsync -r -a -v -e "ssh -oStrictHostKeyChecking=no -i~/.ssh/cs" --delete carsmoney dylan@35.202.94.149:code/CS682
 done
